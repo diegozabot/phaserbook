fb6668a754986ed3b3efbba3b45f67b04e91ae80
Diego <diegozabot@gmail.com>
2017-08-23 22:33:14 -0300

    3

--- cap13-mobile/js/Menu.js
+++ cap13-mobile/js/Menu.js
@@ -5,8 +5,8 @@ Breakout.Menu.prototype = {
         var background = this.add.image(0,0,'background');
         this.add.image(0,0,'menu');
         
-        this.background.inputEnabled = true;
-        this.background.events.onInputDown.add(this.start, this); 
+        background.inputEnabled = true;
+        background.events.onInputDown.add(this.start, this); 
         
         var space = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space.onDown.addOnce(this.start, this);
